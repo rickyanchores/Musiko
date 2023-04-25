470d7038b1318d35493f9fed552ee6a2b6a108fd
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-04-25 18:06:28 +0100

    added styling

--- main.js
+++ main.js
@@ -31,10 +31,12 @@ const fetchData = async () => {
 const renderData = async () => {
 	let data = await fetchData()
   let songs = data.songs[2]
-  let content = `<img src="${songs.header_image_url}"/>
-                  <p>Title: ${songs.full_title}</p>
-  								<p>Artist: ${songs.artist_names}</p>
-                  <p>Url: <a href="${songs.url}" target="_blank">Lyrics</a> </p>`
+  let content = `<div class="Card">
+                    <img src="${songs.header_image_url}"/>
+                    <p>Title: ${songs.full_title}</p>
+                    <p>Artist: ${songs.artist_names}</p>
+                    <p>Url: <a href="${songs.url}" target="_blank">Lyrics</a> </p>
+                  </div>`
   
   
   result.innerHTML = content
